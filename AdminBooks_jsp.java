f2dd7ba7b3f24a5bccb69583e7f6f6cbeb65377a
Alvaro Mendez <amendez@opensolutions.cl>
2020-06-18 15:58:46 -0400

    Update AdminBooks_jsp.java

--- AdminBooks_jsp.java
+++ AdminBooks_jsp.java
@@ -3,7 +3,7 @@ public class AdminBooks_jsp extends HttpJspBase {
 
 //
 //
-//
+//   Prueba 3 de Integracion
 //   Filename: Common.jsp
 //   Generated with CodeCharge  v.1.2.0
 //   JSP.ccp build 05/21/2001
